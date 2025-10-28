04abc4769c3331df560dd3d5af7c8254bac8172c
yazedhamdani <arabsafamily18@gmail.com>
2025-10-28 11:27:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa = document.getElementByid('sapabutton');
+// 2. tambahkan 'even listener' untuk aksi 'click'
+tombolsapa.addEventlistener('click', function() {
+  // 3. jalankan kode ini ketika tombol halo sapa di klick
+                            alert('mantaaaap terima kasih brooo!!!');
+});
